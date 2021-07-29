35032e25aa40629a27f4c3ed6b65befb75c4d376
Arya Kiran <aryakiran@zohomail.eu>
2021-07-29 18:37:01 +0530

    Fixed RSS feed sed cmds

--- rss.sh
+++ rss.sh
@@ -1,3 +1,3 @@
-sed -i '/pubdate/,+2d/g' public/index.xml
-sed -i '/generator/,+1d/' public/index.xml
+sed -i -e '/pubdate/,+2d' public/index.xml
+sed -i -e '/generator/,+1d' public/index.xml
 sed -i 's/^.*<\/item>/<\/item>/g' public/index.xml
