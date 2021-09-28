258e7c2d85ce2985c7e6d6ab7d8284fe3b107d66
Arya Kiran <aryakiran@zohomail.eu>
2021-09-28 12:50:20 +0530

    Clean up UI

--- rss.sh
+++ rss.sh
@@ -1,3 +1,5 @@
 sed -i -e '/pubdate/,+2d' public/index.xml
 sed -i -e '/generator/,+1d' public/index.xml
 sed -i 's/^.*<\/item>/<\/item>/g' public/index.xml
+sed -i '/org-div-home-and-up/,+3d' public/index.html
+sed -i "s/<title>Blog Index/<title>FOSSNix's Blog/g" public/index.html
